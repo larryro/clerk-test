a5a50b829d344acc3653118ecd581e23484ef344
larryro <371767072@qq.com>
2023-03-08 11:58:24 +0800

    change to railway

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://13.229.156.72:8008/api/:path*' // Proxy to Backend
+        destination: 'https://payload-test-production-0c03.up.railway.app/api/:path*' // Proxy to Backend
         // destination: 'http://localhost:3000/api/:path*' // Proxy to Backend
       }
     ]
